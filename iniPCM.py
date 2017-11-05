b79ef412182d7511b98afa29c493b4b97a8e1843
Victor Chura <vmchura@gmail.com>
2017-11-05 08:13:22 -0500

    conScripts

---
+++ iniPCM.py
@@ -0,0 +1,66 @@
+from sgdonpe.mesadepartes.models import RegisteredInstitutions
+from sgdonpe.authentication.models import InternalUser
+from django.contrib.auth.models import User
+from sgdonpe.historiers.models import PrincipalStates
+from sgdonpe.documents.models import TagModel
+
+ENPROYECTO  = PrincipalStates(estado = 'ENPROYEC')
+PARADESPACHO  = PrincipalStates(estado = 'PARADESP')
+EMITIDO  = PrincipalStates(estado = 'EMITIDO')
+RECIBIDO  = PrincipalStates(estado = 'RECIB')
+RECIBIDOPARCIAL  = PrincipalStates(estado = 'RECIBPAR')
+ATENDIDO  = PrincipalStates(estado = 'ATEND')
+ATENDIDOPARCIAL  = PrincipalStates(estado = 'ATENDPAR')
+ARCHIVADO  = PrincipalStates(estado = 'ARCH')
+
+ENPROYECTO.save()
+PARADESPACHO.save()
+EMITIDO.save()
+RECIBIDO.save()
+RECIBIDOPARCIAL.save()
+ATENDIDO.save()
+ATENDIDOPARCIAL.save()
+ARCHIVADO.save()
+
+
+
+tag1 = TagModel(txtTag='Solicita recursos')
+tag2 = TagModel(txtTag='Invitacion evento')
+tag3 = TagModel(txtTag='Decreto urgencia')
+tag4 = TagModel(txtTag='Solicita informacion')
+tag5 = TagModel(txtTag='Informativo')
+
+tag1.save()
+tag2.save()
+tag3.save()
+tag4.save()
+tag5.save()
+
+
+mesa = User.objects.create_user(username='mesadepartes', password='mesadepartes', email='mesadepartes@pcm.com')
+mesa.save()
+
+mesaInterna = InternalUser(user=mesa,nombres='mesa',apellidos='mesa',codigoUsuario='mesa',codDependencia='mesa',dependencia='mesa')
+mesaInterna.save()
+
+
+user = User.objects.create_user(username='Jaspers', password='Jaspers', email='Jaspers@pcm.com')
+user.save()
+
+iu = InternalUser(user=user,nombres='Jaspers',apellidos='Huanay',codigoUsuario='42',codDependencia='IA',dependencia='IA')
+iu.save()
+
+ri = RegisteredInstitutions(institutionName='local',urlInstitution='13.82.181.237',puerto=8000)
+ri.save()
+
+rmayo = RegisteredInstitutions(institutionName='MAYO',urlInstitution='52.170.220.211',puerto=8000)
+rmayo.save()
+
+ronpe = RegisteredInstitutions(institutionName='ONPE',urlInstitution='181.224.226.157',puerto=8000)
+ronpe.save()
+
+
+
+
+
+
